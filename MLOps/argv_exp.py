1e3ecc0c315d500c1949195da601e133ec61c9db
bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2024-06-24 18:26:47 +0530

    mlflow

---
+++ MLOps/argv_exp.py
@@ -0,0 +1,11 @@
+# MLFLOW-Basic-Operation
+
+## For Dagshub
+
+import dagshub
+dagshub.init(repo_owner='Bimbara28', repo_name='e19-co544-cricket-analytics-and-prediction', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
