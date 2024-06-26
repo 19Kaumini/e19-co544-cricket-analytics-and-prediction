993a01f625fbb5393b7efc7864e9d3eb7a866b54
bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2024-06-26 19:28:45 +0530

    API with form

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from MLOps.NRR_prediction import load_data, data_preparation, train_rf, evaluate_model
-import pickle
-import numpy as np
 import os
 from flask_cors import CORS, cross_origin
 import pandas as pd
 from joblib import load
 from flask import Flask, request, jsonify, render_template
-
-
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
@@ -20,7 +15,7 @@ class ClientApp:
         self.model = self.load_model()
 
     def load_model(self):
-        model_path = 'models/model.pkl'
+        model_path = 'models/random_forest_model.pkl'
         return load(model_path)
 
     def predict(self, input_data):
@@ -36,8 +31,33 @@ def home():
 @cross_origin()
 def predict():
     try:
-        json_data = request.get_json()
-        data = pd.DataFrame(json_data)
+        form_data = {
+            'batter': request.form['batter'],
+            'innings': int(request.form['innings']),
+            'wickets_fallen': int(request.form['wickets_fallen']),
+            'bowling_team': request.form['bowling_team'],
+            'batting_team': request.form['batting_team'],
+            'toss_winner': request.form['toss_winner'],
+            'runs_remain': int(request.form['runs_remain']),
+            'first_ball': int(request.form['first_ball']),
+            'current_team_total': int(request.form['current_team_total']),
+            'is_powerplay': int(request.form['is_powerplay']),
+            'Left arm Fast': int(request.form.get('Left arm Fast', 0)),
+            'Left arm Orthodox': int(request.form.get('Left arm Orthodox', 0)),
+            'Left arm Wrist spin': int(request.form.get('Left arm Wrist spin', 0)),
+            'Right arm Fast': int(request.form.get('Right arm Fast', 0)),
+            'Right arm Legbreak': int(request.form.get('Right arm Legbreak', 0)),
+            'Right arm Offbreak': int(request.form.get('Right arm Offbreak', 0)),
+            'venue': request.form['venue']
+        }
+        data = pd.DataFrame([form_data])
+
+        expected_columns = ['batter', 'innings', 'wickets_fallen', 'bowling_team', 'batting_team',
+                            'toss_winner', 'runs_remain', 'first_ball', 'current_team_total',
+                            'is_powerplay', 'Left arm Fast', 'Left arm Orthodox', 'Left arm Wrist spin',
+                            'Right arm Fast', 'Right arm Legbreak', 'Right arm Offbreak', 'venue']
+        data = data[expected_columns]
+
         client_app = ClientApp()
         predictions = client_app.predict(data)
         return jsonify(predictions.tolist())
